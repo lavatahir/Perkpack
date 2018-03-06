f81e19fec10129fad76e3a57f2c3c3a92aac3022
Cyrus Sadeghi <cyrus.cse@gmail.com>
2018-03-06 15:21:43 -0500

    Add PerkRepository

--- src/main/java/perkpack/repositories/PerkRepository.java
+++ src/main/java/perkpack/repositories/PerkRepository.java
@@ -2,10 +2,8 @@ package perkpack.repositories;
 
 import models.Perk;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "perks", path = "perks")
 public interface PerkRepository extends PagingAndSortingRepository<Perk, Long> {
-    Perk findByName(@Param("name") String name);
 }
