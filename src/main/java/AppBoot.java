1309a7be108a51c984f0675d2e0c9291a60a83f1
Cyrus Sadeghi <cyrus.cse@gmail.com>
2018-03-06 14:33:48 -0500

    Restructure directories to work with maven Added SpringBootApplication class (starts Spring boot)

---
+++ src/main/java/AppBoot.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(AppBoot.class);
+    }
+}
