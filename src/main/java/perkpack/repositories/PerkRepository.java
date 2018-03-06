3c161b2257c207e46c7b3f2e168cdcc8d1ff1ca1
Cyrus Sadeghi <cyrus.cse@gmail.com>
2018-03-06 15:38:31 -0500

    PerkController initial implementation, can now add perks through the controller

--- src/main/java/perkpack/repositories/PerkRepository.java
+++ src/main/java/perkpack/repositories/PerkRepository.java
@@ -2,8 +2,10 @@ package perkpack.repositories;
 
 import models.Perk;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "perks", path = "perks")
 public interface PerkRepository extends PagingAndSortingRepository<Perk, Long> {
+    Perk findByName(@Param("name") String name);
 }
