e605b6ce501baed59e342dab50cdcf462c1fe356
Cyrus Sadeghi <cyrus.cse@gmail.com>
2018-03-07 21:23:05 -0500

    Work on upvoting and downvoting perks

--- src/main/resources/static/js/perk.js
+++ src/main/resources/static/js/perk.js
@@ -7,9 +7,43 @@ $(document).ready(function() {
             url: "/perks",
             contentType: "application/json",
             data: '{"name": "' + $("#name").val() + '", "description": "' + $("#description").val() + '"}'
-        }).done(function(msg) {
-            $('#perkTable').append('<tbody/>');
-            $('#perkTable > tbody:last-child').append('<tr><td>' + msg.name + '</td><td>' + msg.description + '</td></tr>');
+        }).done(function(perk) {
+            var perkRow = '<tr class = "perk"><td>' + perk.name + '</td>' + '<td>' + perk.description + '</td>' + '<td>Score:</td><td class = "score">' + perk.score + '</td>';
+
+            perkRow += '<td><input type = "button" value = "Upvote" class = "upvote"/></td>';
+            perkRow += '<td><input type = "button" value = "Downvote" class = "downvote"/></td>';
+            perkRow += '</tr>';
+
+            $(perkRow).appendTo($("#perkTable"));
+
+            $('.perk').on('click', '.upvote, .downvote', function(event) {
+                event.preventDefault();
+
+                $(this).prop("disabled", true);
+                modifyScore($(event.target).closest('tr'), $(this));
+            });
         });
     });
+
+    $(".upvote, .downvote").click(function(event) {
+        event.preventDefault();
+
+        $(this).prop("disabled", true);
+        modifyScore($(event.target).parent());
+    });
+
+    function modifyScore(perk, button) {
+        var score = parseInt($(perk).find('td.score')[0].innerHTML);
+        let eventSource = $($(perk).context);
+
+        console.log(button.innerHTML);
+
+        if (eventSource.hasClass('upvote')) {
+            score++;
+        } else {
+            score--;
+        }
+
+        $(perk).find('td.score')[0].innerHTML = score;
+    }
 });
