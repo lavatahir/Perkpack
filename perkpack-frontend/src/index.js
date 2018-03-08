6a20d081b0f0a67028441a194bcab5d64c7e42d2
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-03-08 11:06:20 -0600

    set up react app with extremely basic placeholder ui

--- perkpack-frontend/src/index.js
+++ perkpack-frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Perkpack from './Perkpack';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Perkpack name="Vanja" />, document.getElementById('root'));
 registerServiceWorker();
