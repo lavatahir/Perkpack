ffea71a6dc90f3dbc5dbd263b96a38c2b263f512
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-03-19 22:20:15 -0400

    set up design language, meant for mobile right now, fetch perks

--- temp/script.js
+++ temp/script.js
@@ -1,5 +1,15 @@
+var populatePerkList = function() {
+	$.getJSON('http://perkpack.herokuapp.com/perks', function(data) {
+		$('#top-perks').empty();
+		var perks = data._embedded.perks;
+		for (var i = 0; i < perks.length; i++) {
+			$('#top-perks').append(
+				'<div class="section perk">'+perks[i].name+'</div>'
+			);
+		}
+	});
+};
+
 $(document).ready(function() {
-	var populatePerkList = function(containerHandle) {
-		
-	};
+	populatePerkList();
 });
