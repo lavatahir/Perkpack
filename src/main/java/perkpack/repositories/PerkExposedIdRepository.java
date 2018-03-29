8bb487ef79ee34cb19248750c6ab485c61f8195f
alifarah <alifarah@cmail.carleton.ca>
2018-03-29 09:40:37 -0400

    exposed the perk id when getting perks

---
+++ src/main/java/perkpack/repositories/PerkExposedIdRepository.java
@@ -0,0 +1,15 @@
+package perkpack.repositories;
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
+import perkpack.models.Perk;
+
+@Component
+public class PerkExposedIdRepository  extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Perk.class);
+    }
+}
