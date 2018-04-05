901a27a7cd3638ef36056703f9e6d9370742dc26
Lava <lavatahir@gmail.com>
2018-04-04 23:07:49 -0400

    cardIds for each user

--- src/main/java/perkpack/models/Account.java
+++ src/main/java/perkpack/models/Account.java
@@ -43,6 +43,8 @@ public class Account {
     @JsonIgnore
     private Set<Card> cards = new HashSet<>();
 
+    private Set<Long> cardIds = new HashSet<>();
+
     @OneToMany(cascade = CascadeType.ALL)
     private Map<Category, CategoryCount> categoryCount = new HashMap<Category, CategoryCount>();
 
@@ -158,11 +160,21 @@ public class Account {
     }
 
     public boolean addCard(Card cardToAdd) {
+        this.cardIds.add(cardToAdd.getId());
         return this.cards.add(cardToAdd);
     }
 
     public boolean removeCard(Card cardToRemove){
+        this.cardIds.remove(cardToRemove.getId());
         return this.cards.remove(cardToRemove);
+    }
+
+    public Set<Long> getCardIds() {
+        return cardIds;
+    }
+
+    public void setCardIds(Set<Long> cardIds) {
+        this.cardIds = cardIds;
     }
 
     private void updateTopCategory() {
